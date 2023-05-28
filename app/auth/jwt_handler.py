02bc159df4c4d4cfaa11f2a1039e86bc4533efb9
affumprince <princeaffumasante@gmail.com>
2023-05-28 19:44:54 +0000

    fix bug

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -15,5 +15,5 @@ def create_access_token(data: dict, expire_delta: timedelta | None = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=[settings.ALGORITHM])
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
